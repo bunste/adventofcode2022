927b5a642111c2049fc1ebebaf93ec671464ffba
Stefan Bungter <stefan.bungter@pressrelations.de>
2022-12-08 10:56:15 +0100

    Added solution for day 1 part 2

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,9 +1,11 @@
 defmodule AdventOfCode.Day1 do
-  def most_calories(calories_list) do
+  def top_calories(calories_list, number_of_elves) do
     calories_list
     |> Enum.chunk_by(&Kernel.is_nil/1)
     |> Enum.filter(&Enum.all?/1)
     |> Enum.map(&Enum.sum/1)
-    |> Enum.max
+    |> Enum.sort
+    |> Enum.take(-1 * number_of_elves)
+    |> Enum.sum
   end
 end
