46d0a7ed53057c69a1187469dc6844465fa098b3
Stefan Bungter <stefan.bungter@pressrelations.de>
2022-12-13 11:04:44 +0100

    Reorganized input files

--- test/day2_test.exs
+++ test/day2_test.exs
@@ -4,19 +4,19 @@ defmodule AdventOfCode.Day2Test do
   @module AdventOfCode.Day2
 
   test "solves a small example" do
-    input = File.read!("test/day2_example.txt")
+    input = File.read!("ressources/day2_example.txt")
 
     assert @module.points_pt1(input) == 15
   end
 
   test "it solves part 1 with the correct solution of 11603" do
-    input = File.read!("test/day2_part1.txt")
+    input = File.read!("ressources/day2_input.txt")
 
     assert @module.points_pt1(input) == 11603
   end
 
   test "it solves part 2 with the correct solution of 12725" do
-    input = File.read!("test/day2_part1.txt")
+    input = File.read!("ressources/day2_input.txt")
 
     assert @module.points_pt2(input) == 12725
   end
--- test/day3_test.exs
+++ test/day3_test.exs
@@ -10,13 +10,13 @@ defmodule AdventOfCode.Day3Test do
   end
 
   test "it solves part 1 with the correct solution of 7691" do
-    input = File.read!("ressources/day3_part1.txt")
+    input = File.read!("ressources/day3_input.txt")
 
     assert @module.prio_score(input) == 7691
   end
 
   test "it solves part 2 with the correct solution of 2508" do
-    input = File.read!("ressources/day3_part1.txt")
+    input = File.read!("ressources/day3_input.txt")
 
     assert @module.prio_score_pt2(input) == 2508
   end
