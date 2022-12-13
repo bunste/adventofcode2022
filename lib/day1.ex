32e97be10a254d3e5ce569aeb0ab999984e186fd
Stefan Bungter <stefan.bungter@pressrelations.de>
2022-12-13 11:19:54 +0100

    Refactored day 1 to work with file inputs

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,11 +1,20 @@
 defmodule AdventOfCode.Day1 do
   def top_calories(calories_list, number_of_elves) do
     calories_list
+    |> String.split("\n")
+    |> Enum.map(&parse/1)
     |> Enum.chunk_by(&Kernel.is_nil/1)
     |> Enum.filter(&Enum.all?/1)
     |> Enum.map(&Enum.sum/1)
     |> Enum.sort(:desc)
     |> Enum.take(number_of_elves)
     |> Enum.sum
+  end
+
+  defp parse(input) do
+    case input do
+      "" -> nil
+      _ -> String.to_integer(input)
+    end
   end
 end
